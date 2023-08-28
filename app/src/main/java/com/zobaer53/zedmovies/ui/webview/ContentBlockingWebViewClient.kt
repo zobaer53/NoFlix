35649faa1c5a9f2b2916e750f440309ac3439183
zobaer53 <hossain.zobaer11858@gmail.com>
2023-08-28 17:37:59 +0600

    fix -- webview proxy addded and adblocker improved

--- app/src/main/java/com/zobaer53/zedmovies/ui/webview/ContentBlockingWebViewClient.kt
+++ app/src/main/java/com/zobaer53/zedmovies/ui/webview/ContentBlockingWebViewClient.kt
@@ -1,5 +1,7 @@
 package com.zobaer53.zedmovies.ui.webview
 
+/*
+
 import android.content.Context
 import android.os.Build
 import android.webkit.WebResourceRequest
@@ -63,3 +65,74 @@ class ContentBlockingWebViewClient(context: Context) : WebViewClient() {
         return url.contains("popup") || url.contains("ad")
     }
 }
+*/
+import android.graphics.Bitmap
+import android.net.Proxy
+import android.os.Build
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import com.google.android.gms.common.api.Response
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+
+class AdBlockingWebViewClient(private val okHttpClient: OkHttpClient) : WebViewClient() {
+
+    override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
+        try {
+            val okHttpRequest = Request.Builder()
+                .url(request?.url.toString())
+                .build()
+
+            val response = okHttpClient.newCall(okHttpRequest).execute()
+            val contentType = response.header("content-type")
+            val inputStream = response.body?.byteStream()
+
+            return if (contentType != null && inputStream != null) {
+                WebResourceResponse(contentType, "UTF-8", inputStream)
+            } else {
+                val adBlocker = AdBlocker()
+
+                if (adBlocker.shouldBlock(request?.url?.toString() ?: "")) {
+                    val emptyStream = ByteArrayInputStream(byteArrayOf())
+                    return WebResourceResponse("text/plain", "utf-8", emptyStream)
+                }
+
+                return super.shouldInterceptRequest(view, request)
+            }
+
+        } catch (e: IOException) {
+            return super.shouldInterceptRequest(view, request)
+        }
+    }
+
+    override fun shouldInterceptRequest(view: WebView?, url: String?): WebResourceResponse? {
+        val adBlocker = AdBlocker()
+
+        if (adBlocker.shouldBlock(url ?: "")) {
+            val emptyStream = ByteArrayInputStream(byteArrayOf())
+            return WebResourceResponse("text/plain", "utf-8", emptyStream)
+        }
+
+        return super.shouldInterceptRequest(view, url)
+    }
+}
+
+class AdBlocker {
+    private val adDomains = setOf(
+        "ad.doubleclick.net",
+        "googleads.g.doubleclick.net",
+        // Add more ad domains as needed
+    )
+
+    fun shouldBlock(url: String): Boolean {
+        return adDomains.any { domain -> url.contains(domain) }
+    }
+}
+
