964690be41fb4e13c5c0034bad7f2834ac2b9386
zobaer53 <hossain.zobaer11858@gmail.com>
2023-08-28 17:37:59 +0600

    fix -- webview proxy addded and adblocker improved

--- app/src/main/java/com/zobaer53/zedmovies/ui/webview/WebView.kt
+++ app/src/main/java/com/zobaer53/zedmovies/ui/webview/WebView.kt
@@ -1,12 +1,18 @@
 package com.zobaer53.zedmovies.ui.webview
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.webkit.WebViewFeature
+import androidx.webkit.WebSettingsCompat
+import okhttp3.OkHttpClient
+import java.net.InetSocketAddress
+
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
@@ -20,9 +26,15 @@ fun VideoWebView(url: String) {
                     javaScriptEnabled = true
                     mediaPlaybackRequiresUserGesture = false
                 }
+                val proxyHost = "91.211.245.176"
+                val proxyPort = 8080 // Your proxy port
+
+                val okHttpClient = OkHttpClient.Builder()
+                    .proxy(java.net.Proxy(java.net.Proxy.Type.HTTP, InetSocketAddress(proxyHost, proxyPort)))
+                    .build()
 
                 // Set WebViewClient and WebChromeClient
-                webViewClient = ContentBlockingWebViewClient(context)
+                webViewClient = AdBlockingWebViewClient(okHttpClient)
                 webChromeClient = WebChromeClient()
 
                 // Load the video URL
@@ -30,4 +42,5 @@ fun VideoWebView(url: String) {
             }
         }
     )
+
 }
