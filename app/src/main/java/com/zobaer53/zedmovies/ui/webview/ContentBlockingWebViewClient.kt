ec47e674bfa02512cbde42d463e53abef9065a0a
zobaer53 <hossain.zobaer11858@gmail.com>
2023-09-12 18:28:11 +0600

    strong adblock used,webview performance improved

--- app/src/main/java/com/zobaer53/zedmovies/ui/webview/ContentBlockingWebViewClient.kt
+++ app/src/main/java/com/zobaer53/zedmovies/ui/webview/ContentBlockingWebViewClient.kt
@@ -1,80 +1,17 @@
 package com.zobaer53.zedmovies.ui.webview
 
-/*
-
-import android.content.Context
-import android.os.Build
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import androidx.annotation.RequiresApi
-
-
-class ContentBlockingWebViewClient(context: Context) : WebViewClient() {
-    override fun shouldInterceptRequest(
-        view: WebView,
-        request: WebResourceRequest
-    ): WebResourceResponse? {
-        val url = request.url.toString()
-
-        // Add your content blocking logic here
-        if (shouldBlock(url)) {
-            // Return an empty response to block the content
-            return WebResourceResponse(null, null, null)
-        }
-
-        // Return null to allow loading the original content
-        return null
-    }
-
-    private val xy = getMiddleScreenPosition(context)
-    override fun onPageFinished(view: WebView?, url: String?) {
-        // Once the page is finished loading, you can scroll to the desired position.
-        view?.scrollTo(xy.first, xy.second)
-    }
-
-    private fun shouldBlock(url: String): Boolean {
-        // Implement your content blocking rules here
-        // Example: Block URLs containing "ad" or "tracking"
-        return url.contains("ad") || url.contains("tracking")
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-        if (request.isRedirect) {
-            // Handle redirects as needed
-            return true
-        }
-
-        val url = request.url.toString()
-        val isPopup = isPopupUrl(url)
-
-        if (isPopup) {
-            // Block popups by returning true
-            return true
-        }
-
-        // Allow loading the original content
-        return false
-    }
-
-    private fun isPopupUrl(url: String): Boolean {
-        // Implement your popup detection logic here
-        // Example: Check if the URL contains "popup" or "ad" keywords
-        return url.contains("popup") || url.contains("ad")
-    }
-}
-*/
 import android.graphics.Bitmap
 import android.net.Proxy
 import android.os.Build
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import com.google.android.gms.common.api.Response
+import io.github.edsuns.adfilter.AdFilter
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.ByteArrayInputStream
@@ -111,17 +48,6 @@ class AdBlockingWebViewClient(private val okHttpClient: OkHttpClient) : WebViewC
             return super.shouldInterceptRequest(view, request)
         }
     }
-
-    override fun shouldInterceptRequest(view: WebView?, url: String?): WebResourceResponse? {
-        val adBlocker = AdBlocker()
-
-        if (adBlocker.shouldBlock(url ?: "")) {
-            val emptyStream = ByteArrayInputStream(byteArrayOf())
-            return WebResourceResponse("text/plain", "utf-8", emptyStream)
-        }
-
-        return super.shouldInterceptRequest(view, url)
-    }
 }
 
 class AdBlocker {
@@ -133,6 +59,22 @@ class AdBlocker {
 
     fun shouldBlock(url: String): Boolean {
         return adDomains.any { domain -> url.contains(domain) }
+    }
+}
+class MyWebClient() : WebViewClient() {
+
+    private val filter = AdFilter.get()
+
+    override fun shouldInterceptRequest(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): WebResourceResponse? {
+        val result = filter.shouldIntercept(view!!, request!!)
+        return result.resourceResponse
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        filter.performScript(view, url)
     }
 }
 
--- app/src/main/java/com/zobaer53/zedmovies/ui/webview/WebView.kt
+++ app/src/main/java/com/zobaer53/zedmovies/ui/webview/WebView.kt
@@ -2,29 +2,59 @@ package com.zobaer53.zedmovies.ui.webview
 
 import android.annotation.SuppressLint
 import android.os.Build
+import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.webkit.WebViewFeature
+import androidx.lifecycle.LifecycleOwner
 import androidx.webkit.WebSettingsCompat
+import io.github.edsuns.adfilter.AdFilter
 import okhttp3.OkHttpClient
 import java.net.InetSocketAddress
 
-
-@SuppressLint("SetJavaScriptEnabled")
+@SuppressLint("SetJavaScriptEnabled", "RequiresFeature")
 @Composable
-fun VideoWebView(url: String) {
+fun VideoWebView(url: String, lifecycleOwner: LifecycleOwner) {
+    val filter = AdFilter.get()
+    val filterViewModel = filter.viewModel
+
+    // Setup AdblockAndroid for your WebView.
+
     AndroidView(
         modifier = Modifier.fillMaxSize(),
         factory = { context ->
             WebView(context).apply {
+                filter.setupWebView(this)
+                // Add filter list subscriptions on first installation.
+                if (!filter.hasInstallation) {
+                    val map = mapOf(
+                        "AdGuard Base" to "https://filters.adtidy.org/extension/chromium/filters/2.txt",
+                        "EasyPrivacy Lite" to "https://filters.adtidy.org/extension/chromium/filters/118_optimized.txt",
+                        "AdGuard Tracking Protection" to "https://filters.adtidy.org/extension/chromium/filters/3.txt",
+                        "AdGuard Annoyances" to "https://filters.adtidy.org/extension/chromium/filters/14.txt",
+                        "AdGuard Chinese" to "https://filters.adtidy.org/extension/chromium/filters/224.txt",
+                        "NoCoin Filter List" to "https://filters.adtidy.org/extension/chromium/filters/242.txt"
+                    )
+                    for ((key, value) in map) {
+                        val subscription = filterViewModel.addFilter(key, value)
+                        filterViewModel.download(subscription.id)
+                    }
+                }
+
+                filterViewModel.onDirty.observe(lifecycleOwner) {
+                    // Clear cache when there are changes to the filter.
+                    // You need to refresh the page manually to make the changes take effect.
+                    clearCache(false)
+                }
+
                 // Enable JavaScript and other settings
                 settings.apply {
                     javaScriptEnabled = true
                     mediaPlaybackRequiresUserGesture = false
+                    WebSettingsCompat.setForceDark(this, WebSettingsCompat.FORCE_DARK_OFF)
                 }
                 val proxyHost = "91.211.245.176"
                 val proxyPort = 8080 // Your proxy port
@@ -34,13 +64,12 @@ fun VideoWebView(url: String) {
                     .build()
 
                 // Set WebViewClient and WebChromeClient
-                webViewClient = AdBlockingWebViewClient(okHttpClient)
-                webChromeClient = WebChromeClient()
+                //webViewClient = AdBlockingWebViewClient(okHttpClient)
+                webViewClient= MyWebClient()
 
                 // Load the video URL
                 loadUrl(url)
             }
         }
     )
-
 }
