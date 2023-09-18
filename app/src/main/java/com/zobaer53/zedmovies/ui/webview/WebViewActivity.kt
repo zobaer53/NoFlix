c33dde4ab4e8524fa13d4a3059b662ef5b19a838
zobaer53 <hossain.zobaer11858@gmail.com>
2023-09-18 12:59:00 +0600

    improved-- webview full screen with screen rotate

--- app/src/main/java/com/zobaer53/zedmovies/ui/webview/WebViewActivity.kt
+++ app/src/main/java/com/zobaer53/zedmovies/ui/webview/WebViewActivity.kt
@@ -1,6 +1,7 @@
 package com.zobaer53.zedmovies.ui.webview
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,14 @@ import io.github.edsuns.adfilter.AdFilter
 class WebViewActivity : ComponentActivity()  {
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    )
             val filter = AdFilter.create(this)
             val movieName = intent.getStringExtra("movieName") ?: ""
             val movieYear = intent.getStringExtra("movieYear") ?: ""
